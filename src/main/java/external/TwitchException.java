79f9d9cba3b90afa668ed757bd179851237bd2bc
lily2019github <lid1991cool@gmail.com>
2021-04-18 22:37:31 -0700

    external file

--- src/main/java/external/TwitchException.java
+++ src/main/java/external/TwitchException.java
@@ -1,1 +1,7 @@
+package com.laioffer.jupiter1.external;
 
+public class TwitchException extends RuntimeException {
+    public TwitchException(String errorMessage) {
+        super(errorMessage);
+    }
+}
